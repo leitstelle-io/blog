7cf12c0fac6adc12ad40c85e1f050585a260ee1e
André Weller <info@lost.design>
2023-08-04 13:47:03 +0200

    feat: init repo

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white h-screen flex justify-center items-center">
+    <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">Koordiniere Notfälle in Echtzeit auf Leitstelle.io 👩‍🚒</h2>
+      <p class="leading-relaxed max-w-2xl">
+        Erlebe auf Leitstelle.io die Faszination des Notfallmanagements: Schlüpfe in die Rolle eines Einsatzleiters, koordiniere Notrufe, weise Einsatzfahrzeuge zu und meistere herausfordernde Szenarien, während du deine Fähigkeiten in einem spannenden Spiel schärfst.
+      </p>
+      <div class="mt-10 flex items-center gap-x-6">
+        <a href="https://discord.gg/cdRq3bztJu" class="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Werde Teil der Community</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
